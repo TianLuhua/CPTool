dad08510bbad13f5dac20cc682d15aa7ddab7798
田露华 <tianluhua@booyue.com.cn>
2020-03-19 18:34:05 +0800

    init project

---
+++ src/main/java/com/booyue/tlh/cptool/CptoolApplication.java
@@ -0,0 +1,13 @@
+package com.booyue.tlh.cptool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CptoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CptoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/booyue/tlh/cptool/CptoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booyue.tlh.cptool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CptoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
